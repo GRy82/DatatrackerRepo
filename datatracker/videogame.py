98d9b37a31b4205c8ab89ee3fb01fd81ffc73176
GRy82 <gwryan@buffalo.edu>
2021-01-25 11:34:12 -0500

    index function had code blocks added to filter data by which consoles have had the largest number of games published. Bar graph also aded to videogame.html page to represent those numbers(top 10).

--- datatracker/videogame.py
+++ datatracker/videogame.py
@@ -1,38 +1,70 @@
 from flask import Flask, jsonify, request, redirect, flash, render_template, url_for, Blueprint
 import requests
+import operator
 
 bp = Blueprint('videogame', __name__)
 
 
-@bp.route('/videogame')
+@bp.route('/videogame', methods=('GET', 'POST'))
 def index():
+    if request.method == 'GET':
+        response = requests.get('https://api.dccresource.com/api/games')
+        all_games = response.json()
 
-    response = requests.get('https://api.dccresource.com/api/games')
-    all_games = response.json()
-    recent_games = []
+        # filter all_games for instructor-given question
 
-    for game in all_games:
-        if game["year"] is not None:
-            if game["year"] >= 2013:
-                recent_games.append(game)
+        recent_games = []
 
-    console_sales = {}
-    for recent_game in recent_games:
-        platform = recent_game["platform"]
-        if recent_game["platform"] not in console_sales.keys():
-            console_sales[platform] = recent_game["globalSales"]
-        else:
-            console_sales[platform] += recent_game["globalSales"]
+        for game in all_games:
+            if game["year"] is not None:
+                if game["year"] >= 2013:
+                    recent_games.append(game)
+
+        console_sales = {}
+        for recent_game in recent_games:
+            platform = recent_game["platform"]
+            if recent_game["platform"] not in console_sales.keys():
+                console_sales[platform] = recent_game["globalSales"]
+            else:
+                console_sales[platform] += recent_game["globalSales"]
+
+        console_name = []
+        console_sales_num = []
+
+        for key in console_sales:
+            console_name.append(key)
+            console_sales_num.append(console_sales[key])
+
+       #################### #################### #################### #################### ####################
+        #filter all_games for custom question
+        #Which console has the highest quantity of games made for it? Top 10
+        console_games = []
+        for game in all_games:
+            console_games.append(game)
+
+        console_pair = {}
+        for game in console_games:
+            platform = game["platform"]
+            if platform not in console_pair:#if console not in dictionary yet, start at count of 1
+                console_pair[platform] = 1
+            else:                               #if key exists, tally the game onto the console_games count.
+                console_pair[platform] += 1
 
 
-    console_name = []
-    console_sales_num = []
+        sorted_pairs = dict(sorted(console_pair.items(), key=operator.itemgetter(1), reverse=True))
 
-    for key in console_sales:
-        console_name.append(key)
-        console_sales_num.append(console_sales[key])
+        top_names = []
+        top_qtys = []
+        counter = 0
+        for name in sorted_pairs:
+            if counter < 10:
+                top_names.append(name)
+                top_qtys.append(sorted_pairs[name])
+                counter += 1
+
+
+        return render_template('videogame/index.html', console_name=console_name, console_sales_num=console_sales_num,
+                               top_names=top_names, top_qtys=top_qtys)
 
 
 
-
-    return render_template('videogame/index.html', console_name=console_name, console_sales_num=console_sales_num)
