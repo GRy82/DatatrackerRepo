55d7d93c80694f032083ed64667ad4da75f9d4d8
GRy82 <gwryan@buffalo.edu>
2021-01-27 10:36:27 -0500

    Pie Chart added. Some minor styling performed. New post flow control added.

--- datatracker/__init__.py
+++ datatracker/__init__.py
@@ -18,6 +18,6 @@ def create_app():
     from . import videogame
     app.register_blueprint(videogame.bp)
 
-    app.add_url_rule('/', endpoint='videogame.index')
+    # app.add_url_rule('/', endpoint='videogame.index')  # only allows get methods.
 
     return app
